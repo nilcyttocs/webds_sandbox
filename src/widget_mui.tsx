8fa82b26949176606bb38ba3ae4d13d411fed086
Scott Lin <scott.lin@tw.synaptics.com>
2022-03-24 00:02:16 -0700

    Add generation of workspace directory tree

--- src/widget_mui.tsx
+++ src/widget_mui.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import TreeView from '@mui/lab/TreeView';
+import TreeItem from '@mui/lab/TreeItem';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 import Typography from '@mui/material/Typography';
 import { ThemeProvider } from '@mui/material/styles';
 
+import { requestAPI } from './handler';
+
+const WORKSPACE_DIR = '%2Fhome%2Fpi%2Fjupyter%2Fworkspace';
+
 export const FoobarMui = (props:any): JSX.Element => {
+  const [workspaceDir, setWorkspaceDir] = useState<any>(null);
+
+  const getWorkspaceDir = async () => {
+    try {
+      const dir = await requestAPI<any>('filesystem?dir=' + WORKSPACE_DIR);
+      setWorkspaceDir(dir)
+      console.log(dir);
+    } catch (error) {
+      console.error(`Error - GET /webds/filesystem?dir=${WORKSPACE_DIR}\n${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getWorkspaceDir();
+  }, []);
+
+  let nodeID = 0;
+  const generateTree = (node: any): JSX.Element => {
+    nodeID += 1;
+    return(
+      <TreeItem nodeId={nodeID.toString()} label={node.name}>
+        {Array.isArray(node.children) ? node.children.map((node: any) => generateTree(node)) : null}
+      </TreeItem>
+    );
+  };
+
   return (
     <ThemeProvider theme={props.theme}>
       <div>
@@ -12,6 +47,18 @@ export const FoobarMui = (props:any): JSX.Element => {
         >
           Foobar
         </Typography>
+        {workspaceDir ? (
+          <TreeView
+            multiSelect
+            defaultCollapseIcon={<ExpandMoreIcon/>}
+            defaultExpandIcon={<ChevronRightIcon/>}
+            sx={{height: 240, maxWidth: 400}}
+          >
+            {generateTree(workspaceDir)}
+          </TreeView>
+        ) : (
+          null
+        )}
       </div>
     </ThemeProvider>
   );
